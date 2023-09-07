192191f1c87663f5b5b99a6e3ca948c61d645886
miuractech <miuractech@gmail.com>
2023-09-07 13:34:10 +0000

    to do list

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,13 +1,22 @@
 import { Button } from "@mantine/core";
-import { Demo } from "./Select";
+import DndListHandle from "./dnd";
 
 export default function Component() {
-  return (
-    <div>
-    <div  className="text-3xl font-bold" >
-    To Do list
-    </div>
-      <Demo />
-      </div>
-  )
+    return (
+        <div>
+            <div style={{fontSize:25,fontWeight:600}} >
+                To Do list
+            </div>
+
+            {data.map(d=>(
+                <div>
+                {d.name}
+                </div>
+            ))}
+        </div>
+    )
 }
+//   <Demo />
+
+
+export const data = [{ position: 1, mass: 1.007, symbol: "H", name: "Hydrogen" }, { position: 6, mass: 12.011, symbol: "C", name: "Carbon" }, { position: 8, mass: 15.999, symbol: "O", name: "Oxygen" }, { position: 11, mass: 22.99, symbol: "Na", name: "Sodium" }, { position: 16, mass: 32.06, symbol: "S", name: "Sulfur" }]; 
