81f96197eaa7ebf729082467daf654739f6e80a4
miuractech <miuractech@gmail.com>
2023-09-12 08:01:52 +0530

    updated

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,22 +1,16 @@
-import { Button } from "@mantine/core";
-import DndListHandle from "./dnd";
+import logo from "../YOCO.svg";
 
 export default function Component() {
-    return (
-        <div>
-            <div style={{fontSize:25,fontWeight:600}} >
-                To Do list
-            </div>
-
-            {data.map(d=>(
-                <div>
-                {d.name}
-                </div>
-            ))}
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <img src={logo} alt="" />
+    </div>
+  );
 }
-//   <Demo />
-
-
-export const data = [{ position: 1, mass: 1.007, symbol: "H", name: "Hydrogen" }, { position: 6, mass: 12.011, symbol: "C", name: "Carbon" }, { position: 8, mass: 15.999, symbol: "O", name: "Oxygen" }, { position: 11, mass: 22.99, symbol: "Na", name: "Sodium" }, { position: 16, mass: 32.06, symbol: "S", name: "Sulfur" }]; 
